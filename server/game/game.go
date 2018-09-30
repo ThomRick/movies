d1ac779bc6c3deec0e97fbfef128a09c4bd3cbbe
Matthieu Mouminoux <mmouminoux@mirakl.com>
2018-09-30 17:45:15 +0200

    Add 3 movies and HTML5 movie player

--- server/game/game.go
+++ server/game/game.go
@@ -8,10 +8,10 @@ import (
 	"time"
 )
 
-const MovieDuration = 5000
+const MovieDuration = 30000
 
 type Game struct {
-	movies       []string
+	movies       []movieInfo
 	currentMovie atomic.Value
 	players      atomic.Value
 	playerMux    sync.Mutex
@@ -22,7 +22,13 @@ type Player struct {
 	Score *int
 }
 
+type movieInfo struct {
+	title string
+	file  string
+}
+
 type Movie struct {
+	File          string
 	Title         string
 	MsToNextMovie int64
 }
@@ -34,7 +40,10 @@ type movie struct {
 
 func New() *Game {
 	g := Game{
-		movies: []string{"movie 1", "movie 2", "movie 3"},
+		movies: []movieInfo{movieInfo{title: "Fight Club", file: "movie1.mp4"},
+			movieInfo{title: "American Psyco", file: "movie2.mp4"},
+			movieInfo{title: "American History X", file: "movie3.mp4"},
+		},
 	}
 
 	go func() {
@@ -59,9 +68,10 @@ func New() *Game {
 	return &g
 }
 
-func (g *Game) GetCurrentMovie() Movie {
+func (g *Game) GetCurrentMovie() *Movie {
 	m := g.currentMovie.Load().(movie)
-	return Movie{Title: g.movies[m.idx], MsToNextMovie: MovieDuration - time.Now().Sub(m.currentStartTime).Nanoseconds()/int64(time.Millisecond)}
+	movieInfo := g.movies[m.idx]
+	return &Movie{Title: movieInfo.title, File: movieInfo.file, MsToNextMovie: MovieDuration - time.Now().Sub(m.currentStartTime).Nanoseconds()/int64(time.Millisecond)}
 }
 
 func (g *Game) NewPlayer(playerName string) error {
@@ -76,20 +86,20 @@ func (g *Game) NewPlayer(playerName string) error {
 	return nil
 }
 
-func (g *Game) GetPlayers() []Player {
+func (g *Game) GetPlayers() *[]Player {
 	var playerList []Player
 	players := g.getPlayers()
 	for k := range players {
 		playerList = append(playerList, players[k])
 	}
-	return playerList
+	return &playerList
 }
 
 func (g *Game) GuessTitle(playerName string, titleName string) string {
 	g.playerMux.Lock()
 	m := g.currentMovie.Load().(movie)
 	var message string
-	if strings.EqualFold(g.movies[m.idx], titleName) {
+	if strings.EqualFold(g.movies[m.idx].title, titleName) {
 		*(g.getPlayers()[playerName].Score)++
 		message = "ok"
 	} else {
